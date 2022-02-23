6ca638eeb21b03324337d2e69e2d82e5b9bfc056
Gabriel Gitonga <gabworks51@gmail.com>
2022-02-24 00:22:20 +0300

    Adding producs orders and cart cruds

--- index.js
+++ index.js
@@ -10,14 +10,24 @@ const db = require('./config/db')
 
 const Users = require('./routes/User')
 const Auth = require('./routes/Auth')
-
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+// const stripeRoute = require("./routes/stripe");
 db()
 
 
+//app middlewares
+app.use(cors());
 app.use(express.json())
 
-app.use("/api/users", Users)
+
+//routes middlewares 
 app.use("/api/auth", Auth)
+app.use("/api/users", Users)
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
 
 app.listen(process.env.PORT || 4000, () => console.log("Server running at port : " +
     process.env.PORT))
--- routes/User.js
+++ routes/User.js
@@ -1,6 +1,6 @@
 const User = require("../models/User");
+const CryptoJS = require("crypto-js")
 const {
-    verifyToken,
     verifyTokenAndAuthorization,
     verifyTokenAndAdmin,
 } = require("./verifyToken");
